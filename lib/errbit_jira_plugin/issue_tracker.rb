84ed08c263917118a4e46c783ea86ae09cc80ac1
Matthew McFarling <matthew.mcfarling@gmail.com>
2014-10-21 17:14:35 -0400

    updating plugin values

--- lib/errbit_jira_plugin/issue_tracker.rb
+++ lib/errbit_jira_plugin/issue_tracker.rb
@@ -103,15 +103,9 @@ module ErrbitJiraPlugin
         issue_title =  "[#{ problem.environment }][#{ problem.where }] #{problem.message.to_s.truncate(100)}".delete!("\n")
         issue_description = self.class.body_template.result(binding).unpack('C*').pack('U*')
         
-        issue = {"fields"=>{"summary"=>issue_title, "description"=>issue_description, "assignee"=>{}, "components"=>{}, "project"=>{"key"=>params['project_id']},"issuetype"=>{"name"=>params['issue_type']},"priority"=>{"name"=>params['issue_priority']}}}
-        
-        if params['account']
-          issue[:fields][:assignee] = {:name => params['account']}
-        end
-
-        if params['issue_component']
-          issue[:fields][:components] = {:name => params['issue_component']}
-        end
+        issue = {"fields"=>{"summary"=>issue_title, "description"=>issue_description,"project"=>{"key"=>params['project_id']},"issuetype"=>{"name"=>params['issue_type']},"priority"=>{"name"=>params['issue_priority']}}}
+        #issue[:fields][:assignee] = {:name => params['account']} if params['account']
+        #issue[:fields][:components] = {:name => params['issue_component']} if params['issue_component']
 
         issue_build = client.Issue.build
         issue_build.save(issue)
